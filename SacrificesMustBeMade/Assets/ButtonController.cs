5356cb8b204cbbdcfd54be29a1c7c2e11036c838
Shellest <szelest88@gmail.com>
2018-12-01 18:30:38 +0100

    Create more consistent interface.

--- SacrificesMustBeMade/Assets/ButtonController.cs
+++ SacrificesMustBeMade/Assets/ButtonController.cs
@@ -1,18 +1,25 @@
 ﻿using Model;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ButtonController : MonoBehaviour
 {
 
+    public BarsManager barsManager;
     public bool isActive;
 
     public GameEventActionType gameEventActionType;
+    public TextMeshPro testTMP;
 
     void OnMouseDown()
     {
-        Debug.LogError("" + gameEventActionType);
+        string res = "" + gameEventActionType + " has been induced";
+
+        testTMP.text = res;
+
+        barsManager.setSomeTestValues();
         // load a new scene
     }
 }
