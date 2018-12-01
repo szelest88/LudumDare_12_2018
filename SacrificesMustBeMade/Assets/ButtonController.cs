bec46a205c1bad6e9a184c121fd73efedb15755e
Shellest <szelest88@gmail.com>
2018-12-01 20:38:42 +0100

    Make some UI adjustments.

--- SacrificesMustBeMade/Assets/ButtonController.cs
+++ SacrificesMustBeMade/Assets/ButtonController.cs
@@ -15,6 +15,13 @@ public class ButtonController : MonoBehaviour
 
     void OnMouseDown()
     {
+        GetComponent<SpriteRenderer>().color = Color.gray;
+    }
+
+    private void OnMouseUp()
+    {
+
+        GetComponent<SpriteRenderer>().color = Color.white;
         string res = "" + gameEventActionType + " has been induced";
 
         Debug.LogError("REMOVE THIS MOTHERFUCKER!");
