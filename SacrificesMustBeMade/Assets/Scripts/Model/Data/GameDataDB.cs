04e72c0bd3e7f31d9bd7d475c125fa1fa37c7b1e
Silnar <m.zdankiewicz@gmail.com>
2018-12-02 12:14:44 +0100

    GameDataDB: Sync events.

--- SacrificesMustBeMade/Assets/Scripts/Model/Data/GameDataDB.cs
+++ SacrificesMustBeMade/Assets/Scripts/Model/Data/GameDataDB.cs
@@ -9,7 +9,31 @@ namespace Model
 
         public static GameEvent[] GameEvents = new GameEvent[] {
             // GameEventTemplate.Event,
-            // GameEventFanatyzm.Event
+            // GameEventFanatyzm.Event,
+            GameEventBaseAttack.Event,
+            GameEventBuildAltar.Event,
+            GameEventCrowdedHallway.Event,
+            GameEventCultBaseFound.Event,
+            GameEventCultRadicalization.Event,
+            GameEventDebauchery.Event,
+            GameEventDisease.Event,
+            GameEventEmptySack.Event,
+            GameEventFalseGodWorhippers.Event,
+            GameEventFanatyzm.Event,
+            GameEventFreshSouls.Event,
+            GameEventGetRelic.Event,
+            GameEventGetVirgin.Event,
+            GameEventHornOfPlenty.Event,
+            GameEventKleptomaniacs.Event,
+            GameEventMuseumSale.Event,
+            GameEventNewRecruits.Event,
+            GameEventRitualTheory.Event,
+            GameEventSaleAtTheForbiddenScrolls.Event,
+            GameEventSlackingFollowers.Event,
+            GameEventSplit.Event,
+            GameEventSuicidalThoughts.Event,
+            GameEventTemplate.Event,
+            GameEventUngratefulWorshipers.Event,
         };
 
     }
