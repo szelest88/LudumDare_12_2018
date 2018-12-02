037ce701e365c44b8bc1f8f8fd39b6a3b59ce5a4
Sanic <krzysiek56000@gmail.com>
2018-12-02 14:51:18 +0100

    New events

---
+++ SacrificesMustBeMade/Assets/Scripts/Model/Data/GameEventNewContacts.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model
+{
+    public class GameEventNewContacts
+    {
+        public static GameEvent Event = new GameEvent
+        {
+            Name = "New Contacts",
+            Predicate = gs => gs.Res.Cultists <= 80,
+            Priority = 5,
+            MinTurn = 1,
+            Retention = 7,
+            Description = "A new bar has opened nearby. We could start recruiting new followers there. Or, we could start kidnapping drunk people. Or maybe even paying the owner to deliver us those, who fall in debt with him.",
+            Actions = new[] {
+                new GameEventAction {
+                    Type = GameEventActionType.Conflict,
+                    Description = "Dragging some drunk, unconscious bodies at night is apparently very good way of gaining new followers. They should open new bars more often.",
+                    Effect = gs => {
+                        gs.Res.Cultists += 10;
+                        return gs;
+                    }
+                },
+                new GameEventAction {
+                    Type = GameEventActionType.Enthusiasm,
+                    Description = "Many people are willing to join a cult, as long, as it gives them a meaning in life. Sad, but true. But, talking about secret cults in bars makes them less secret.",
+                    Effect = gs => {
+                        gs.Res.Cultists += 20;
+                        gs.Res.Notority += 20;
+                        return gs;
+                    }
+                },
+                new GameEventAction {
+                    Type = GameEventActionType.Diplomacy,
+                    Description = "A bar in this part of town has many clients, who fall in debt. We pay them a simple offer: When a client cannot pay, we pay for the client, and the client is delivered to us. And a small extra pay, so the bar owner doesn't ask any questions.",
+                    Effect = gs => {
+                        gs.Res.Wealth -= 20;
+                        gs.Res.Cultists += 30;
+                        gs.Res.Notority += 10;
+                        return gs;
+                    }
+                }
+            }
+        };
+
+    }
+
+}
---
+++ SacrificesMustBeMade/Assets/Scripts/Model/Data/GameEventOtherCultBreakdown.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model
+{
+    public class GameEventOtherCultBreakdown
+    {
+
+        public static GameEvent Event = new GameEvent
+        {
+            Name = "Other Cult Breakdown",
+
+            Predicate = gs => gs.Res.Notority >= 60,
+            Priority = 5,
+            MinTurn = 5,
+            Retention = 10,
+            Description = "A messenger from a different cult has arrived, informing us, their cult is breaking down. The Inquisition is on their tails, and it is only a matter of time, before their base gets burnt to the ground. He asks, if some of them could join us. I worry, that they would bring some inquisitors with them. Metaphorically.",
+            Actions = new[] {
+                new GameEventAction {
+                    Type = GameEventActionType.Enthusiasm,
+                    Description = "Large number of new cultists joined our ranks. And there are no inquisitors in sight. Yet.",
+                    Effect = gs => {
+                        gs.Res.Cultists += 40;
+                        gs.Res.Notority += 20;
+                        return gs;
+                    }
+                },
+                new GameEventAction {
+                    Type = GameEventActionType.Diplomacy,
+                    Description = "We order them not to draw any attention. Later, a small number of cultists join our cause. They promise, they have not been followed.",
+                    Effect = gs => {
+                        gs.Res.Cultists += 20;
+                        return gs;
+                    }
+                },
+
+                new GameEventAction
+                {
+                    Type = GameEventActionType.Apathy,
+                    Description = "We agree to accept this one man. The rest has to worry for themselves",
+                    Effect = gs => {
+                        gs.Res.Cultists += 1;
+                        return gs;
+                    }
+                }
+            }
+        };
+
+    }
+
+}
