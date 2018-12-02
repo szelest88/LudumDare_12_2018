4497c15b583e89b42e0711601314ac8143b22ed4
Krzysztof Szymanowski <krzysztof.szymanowski@gecko-lab.com>
2018-12-02 13:06:13 +0100

    music WIP

---
+++ SacrificesMustBeMade/Assets/MusicController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicController : MonoBehaviour {
+
+    public AudioClip tutorial;
+    public AudioClip[] buttons;
+    public AudioClip whispers;
+    public AudioMixer mixer;
+
+
+    AudioSource button;
+    AudioSource whisper;
+    AudioSource tut;
+    void Start()
+    {
+        button = gameObject.AddComponent<AudioSource>();
+        whisper = gameObject.AddComponent<AudioSource>();
+        tut = gameObject.AddComponent<AudioSource>();
+
+        button.outputAudioMixerGroup = mixer.FindMatchingGroups("Button")[0];
+        tut.outputAudioMixerGroup = mixer.FindMatchingGroups("Tutorial")[0];
+        whisper.outputAudioMixerGroup = mixer.FindMatchingGroups("Whispers")[0];
+
+        tut.clip = tutorial;
+        tut.Play();
+    }
+
+    public void TutorialEnd()
+    {
+        tut.Stop();
+        whisper.clip = whispers;
+        whisper.Play();
+    }
+
+    public void PlayClick()
+    {
+        button.clip = buttons[Random.Range(0, buttons.Length)];
+        button.loop = false;
+        button.Play();
+    }
+}
