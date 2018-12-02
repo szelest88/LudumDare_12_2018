a8e0d11fc614e4a6ad43fde2f32b1e63c377e787
Krzysztof Szymanowski <krzysztof.szymanowski@gecko-lab.com>
2018-12-02 02:06:16 +0100

    Get relic

---
+++ SacrificesMustBeMade/Assets/Scripts/Model/Data/GameEventGetRelic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model
+{
+    public class GameEventGetRelic
+    {
+        public static GameEvent Event = new GameEvent
+        {
+            Name = "Get Relic",
+            Predicate = gs => gs.Res.Wealth >= 125 || gs.Res.Cultists >= 125,
+            Priority = 10,
+            MinTurn = 10,
+            Retention = 8,
+            Description = "News of ancient temple spreads among University scholars. Description resembles temple depicted in 'The Book of Distant Shores'. Book says of dagger forged under red moon. Shall we send most devoted brothers on expedition. On the other hand University archaeologist are eager to go. We could aid them through dangers of dwelling within temples erected in names of The Elder Ones and steal most sought possesion right from their noses.",
+            Actions = new[] {
+                new GameEventAction {
+                    Type = GameEventActionType.Enthusiasm,
+                    Description = "Many brothers commited their flesh to couse. Scourge inflicted upon them by protective seals placed within temple meant nothing as long as it brings us closer to Your forthcoming Dark One",
+                    Effect = gs => {
+                        gs.Res.Cultists -= 70;
+                        gs.Res.Zeal += 40;
+                        return gs;
+                    }
+                },
+                new GameEventAction {
+                    Type = GameEventActionType.Ruse,
+                    Description = "To think they dare to call themselves scholars. They know nothing. We led them forth. Using them to focus wrath of Protectors on them. Their numbers dwindled fast but prospect of glory and faith made them walk over bodies of fallen. But prize at the end was never meant for them.",
+                    Effect = gs => {
+                        gs.Res.Notority += 60;
+                        gs.Res.Cultists -= 30;
+                        gs.Res.Wealth -=30;
+                        return gs;
+                    }
+                }
+            }
+        };
+
+    }
+
+}
